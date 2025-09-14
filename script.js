1042d70a90d39bc7c02b5193775673dae663594b
Arvin Dave Calio <arvindavecalio37@gmail.com>
2025-09-14 23:47:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function openModal(imgSrc, title) {
-  document.getElementById("modal").style.display = "block";
-  document.getElementById("modalImage").src = imgSrc;
-  document.getElementById("caption").innerText = title;
+// Projects modal
+function openModal(imageSrc, captionText) {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modalImage");
+  const caption = document.getElementById("caption");
+
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+  caption.innerText = captionText;
 }
 
 function closeModal() {
-  document.getElementById("modal").style.display = "none";
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
 }
+
+// Close project modal when clicking outside image
+window.addEventListener("click", (e) => {
+  const modal = document.getElementById("modal");
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
